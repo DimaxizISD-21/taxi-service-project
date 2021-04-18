4ba1f5a5465b5c6268aee4f9053034581ea37e14
Oleksiy Nikolaenko <alosha.nikolanko333@gmail.com>
2021-04-18 09:32:41 +0300

    fix

--- taxi_service/accounts/urls.py
+++ taxi_service/accounts/urls.py
@@ -14,9 +14,5 @@ urlpatterns = [
     path('dispatcher_logout/', DispatcherLogoutView.as_view(), name='dispatcher_logout'),
     path('dispatcher_register/', DispatcherRegisterView.as_view(), name='dispatcher_register'),
     path('dispatcher_profile/', DispatcherProfileView.as_view(), name='dispatcher_profile'),
-    # path('dispatcher_profile/remove_order/', RemoveTaxiOrder.as_view(), name='remove_order'),
-    # path('dispatcher_profile/remove/<int:order_id>/', removeTaxiOrder, name='remove_order'),
-    # path('dispatcher_profile/remove_order/<int:order_id>/', removeTaxiOrder, name='remove_order'),
     path('dispatcher_profile/remove_order/<int:order_id>/', RemoveTaxiOrder.as_view(), name='remove_order'),
-    # path('dispatcher_profile/remove_order/<int:order_id>/<int:taxi_auto>', removeTaxiOrder, name='remove_order'),
 ]
